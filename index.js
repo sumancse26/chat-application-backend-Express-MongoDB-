c208dc113943454032969f3567a3582c70250501
Suman Sarkar <suman.iu32@gmail.com>
2025-01-06 12:31:07 +0600

    fix: remove unused code

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const dotEnv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const {
-  errorHandler,
-  notFoundHandler,
-} = require("./middlewares/common/errorHandler.js");
+
 const loginRouter = require("./router/loginRouter");
 const usersRouter = require("./router/usersRouter");
 const inboxRouter = require("./router/inboxRouter.js");
@@ -44,12 +41,6 @@ app.use(loginRouter);
 app.use(usersRouter);
 app.use(inboxRouter);
 app.use(messageRouter);
-
-//error handling
-//common error handler
-// app.use(errorHandler);
-//404 not found
-// app.use(notFoundHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
