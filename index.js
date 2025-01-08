010a73406a73c14c53e55eb8393ba6491272d89d
Suman Sarkar <suman.iu32@gmail.com>
2025-01-08 09:37:29 +0600

    fix: cors

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const messageRouter = require("./router/messageRouter.js");
 const app = express();
 dotEnv.config();
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credential: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-};
-app.options("", cors(corsOptions));
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+// };
+// app.options("", cors(corsOptions));
+app.use(cors());
 
 //database connection
 mongoose
