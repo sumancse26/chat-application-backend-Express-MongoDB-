31c04c19417d4eb7ba697993b8f774a4fbb2882e
Suman Sarkar <suman.iu32@gmail.com>
2025-01-09 12:14:53 +0600

    fix: resolve user password issue

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,7 +6,7 @@ const login = async (req, res) => {
   try {
     const user = await User.findOne({
       $or: [{ email: req.body.username }, { mobile: req.body.username }],
-    }).select({ __v: 0, password: 0 });
+    }).select({ __v: 0 });
 
     const validPassword = await bcrypt.compare(
       req.body.password,
@@ -25,7 +25,14 @@ const login = async (req, res) => {
       status: 200,
       message: "User logged in successfully",
       token: token,
-      user: user,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        mobile: user.mobile,
+        role: user.role,
+        updatedAt: user.updatedAt,
+      },
     });
   } catch (e) {
     res.json({
